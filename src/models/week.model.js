02637cbf49aac4681bd1609245f6afc5c6e2bbe4
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-03-11 11:12:30 +0100

    models: modificato i nomi delle funzioni di week.model.js

--- src/models/week.model.js
+++ src/models/week.model.js
@@ -2,7 +2,7 @@
 const db = require("../config/db");
 
 // creo una nuova settimana e restituisco l'id appena generato dal database
-exports.createTeam = async (weekNumber) => {
+exports.createWeek = async (weekNumber) => {
   // inserisco la nuova settimana con i campi ricevuti come parametri
   const [result] = await db.query(`INSERT INTO weeks (week_number) VALUES (?)`, [weekNumber]);
   // restituisco l'id della nuova settimana appena creata
@@ -10,7 +10,7 @@ exports.createTeam = async (weekNumber) => {
 };
 
 // recupero la lista di tutte le settimane
-exports.showTeams = async () => {
+exports.showWeeks = async () => {
   // query per estrapolare numero settimana e id delle settimane dalla tabella weeks
   const [rows] = await db.query("SELECT * FROM weeks");
   return rows;
