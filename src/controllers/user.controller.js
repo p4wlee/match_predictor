d2039dfd170b46f7904be1231c88e500d37ade32
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-03-13 10:17:48 +0100

    controllers: aggiunta funzione getUserById in user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,3 @@
-// importo bcrypt per hashare le password
-const bcrypt = require("bcrypt");
-// importo jsonwebtoken per generare e verificare i token
-const jwt = require("jsonwebtoken");
 // importo dotenv per leggere le variabili dal file .env
 require("dotenv").config();
 // importo userModel per interagire con la tabella users nel database
@@ -12,6 +8,36 @@ exports.getMe = async (req, res) => {
   try {
     // leggo l'id dell'utente da req.user, popolato dal middleware auth.js
     const userId = req.user.id;
+
+    // cerco l'utente nel database tramite id
+    const user = await userModel.findById(userId);
+
+    // se l'utente non esiste rispondo con 404
+    if (!user) {
+      return res.status(404).json({
+        message: `user not found`,
+      });
+    }
+
+    // escludo i campi sensibili dalla risposta
+    // password e refresh token non devono mai essere mandati al client
+    const { password, refresh_token, refresh_token_expires_at, ...publicData } = user;
+
+    // rispondo con i dati pubblici dell'utente
+    return res.status(200).json({ publicData });
+  } catch (error) {
+    // gestisco qualsiasi errore imprevisto del server
+    return res.status(500).json({
+      message: `internal server error`,
+    });
+  }
+};
+
+// restituisco i dati pubblici di un utente specifico tramite id
+exports.getUserById = async (req, res) => {
+  try {
+    // leggo l'id dell'utente dall'url (es. /users/5)
+    const userId = req.params.id;
 
     // cerco l'utente nel database tramite id
     const user = await userModel.findById(userId);
