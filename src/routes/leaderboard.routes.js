af3621440c2558de7ce53d721da38da0837268e6
Davide Paulicelli <paulicelli.davide@gmail.com>
2026-03-24 10:41:58 +0100

    routes: rinominata variabile leaderBoardController in leaderboardController per coerenza

--- src/routes/leaderboard.routes.js
+++ src/routes/leaderboard.routes.js
@@ -3,11 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 // importo il controller per la gestione della classifica
-const leaderBoardController = require("../controllers/leaderboard.controller");
+const leaderboardController = require("../controllers/leaderboard.controller");
 
 // route pubblica per ottenere la classifica generale con filtri opzionali
 // es. /leaderboard?week=3&sort=asc
-router.get("/", leaderBoardController.getLeaderboard);
+router.get("/", leaderboardController.getLeaderboard);
 
 // esporto il router per montarlo in app.js
 module.exports = router;
